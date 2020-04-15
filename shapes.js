ae0355da524a3d0a4357f445328a332996e98ba0
Ryan Whitmore <ethrealprojections@gmail.com>
2020-04-15 09:30:17 -0700

    Added movable player and extended Rex to a dim wall

--- shapes.js
+++ shapes.js
@@ -8,6 +8,17 @@ let edge = 50;
 
 console.log('canvas dimentions: ' + screenWidth + ' x ' + screenHeight);
 
+var keyclick = {};
+document.addEventListener("keydown", function(event){
+    keyclick[event.keyCode]=true;
+    console.log(keyclick);
+    move(keyclick);
+}, false);
+document.addEventListener("keyup", function(event){
+    delete keyclick[event.keyCode];
+}, false);
+
+
 class Square {
     constructor(x, y, width, height, speed, color) {
         this.x = x;
@@ -61,6 +72,17 @@ class Circle {
         this.y += this.speedy;
     }
 }
+function move(keyclick){
+    if(37 in keyclick){player.x -= player.speedx;}
+    if(38 in keyclick){player.y -= player.speedy;}
+    if(39 in keyclick){player.x += player.speedx;}
+    if(40 in keyclick){player.y += player.speedy;}
+    
+    if(player.x >= (canvas.width-32)){player.x=0;}
+    if(player.y >= (canvas.height-32)){player.y=0;}
+    if(player.x < 0){player.x=(canvas.width-32);}
+    if(player.y < 0){player.y=(canvas.height-32);}
+}
 
 var ball = new Circle(20, 20, 10, 2, 2, "#46F703");
 console.log(ball);
@@ -75,11 +97,19 @@ var updateBall = function() {
     ball.bounce();
 }
 
+//Player
+var player = new Circle(20, 20, 10, 9, 9, "#751490");
+var drawPlayer = function(){
+    ctx.beginPath();
+    ctx.arc(player.x, player.y, player.radius, 0, Math.PI*2);
+    ctx.fillStyle = player.color;
+    ctx.fill();
+    ctx.closePath();
+}
 
-
-// rex is a single stationary square
+// rex is the last tree on this planet and has no branches left
 var rex = new Square(
-   ( (screenWidth/2)-50), ((screenHeight/2)-50), edge, edge, 0, "#03F7EE"
+   ( (screenWidth/2)-50), ((screenHeight/2)-50), 20, 300, 0, "#101306"
 );
 console.log(rex);
 var drawRex = function(){
@@ -88,12 +118,13 @@ var drawRex = function(){
     ctx.fillRect(rex.x, rex.y, rex.width, rex.height);
 }
 
-// sprites will be multiple squares moving up and down
+// sprites will be multiple squares all over the place
 var sprites = [
     new Square(5, 270, edge, edge, 2, "#FF5733"),
     new Square(120, 300, edge, edge, 1, "#900C3F"),
     new Square(350, 100, edge, edge, 3, "#FFC300"),
-    new Square(200, 100, edge, edge, 10, "#71a87c")
+    new Square(200, 100, edge, edge, 4, "#71a87c"),
+    new Square(280, 70, edge, edge, 3, "#A9C23F")
 ];
 
 console.log(sprites);
@@ -117,25 +148,30 @@ var updateSprites = function() {
 
 function collisionDetection(){
         for(let i=0; i<sprites.length; i++){ 
-        var s = sprites;
-        if(s[i].x < ball.x + ball.radius &&
-        s[i].x + edge > ball.x &&
-        s[i].y < ball.y + ball.radius &&
-        s[i].y + edge > ball.y)
-        {
-            console.log('Collision'); 
-        }
+            let s = sprites;
+                var c = 0;
+            function calculateCollision(){
+                if(s[i].x < ball.x + ball.radius &&
+                s[i].x + edge > ball.x &&
+                s[i].y < ball.y + ball.radius &&
+                s[i].y + edge > ball.y){
+                    console.log('Collision');
+                    ball.color = "#6f1490";
+        } else{ball.color = "#46F703"}
+            }
+            calculateCollision();
     }
 }
 
 // Run the Game
 var gameLoop = function() {
     ctx.clearRect(0, 0, screenWidth, screenHeight);
-    //drawRex();
+    drawRex();
     updateSprites();
     drawSprites();
     updateBall();
     drawBall();
+    drawPlayer();
     collisionDetection();
     window.requestAnimationFrame(gameLoop);
 }
